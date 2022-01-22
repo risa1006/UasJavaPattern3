bf102e3e0c5ab0066e5a1513e5abda42875366ea
risa1006 <risa@unida.ac.id>
2022-01-22 09:11:26 +0700

    JavaPattern3
    membuat java pattern 3

---
+++ src/JavaPattern3.java
@@ -0,0 +1,22 @@
+
+public class JavaPattern3 {
+		public static void main(String[] args) {
+			int x;
+			int y; 
+			int a = 10;
+			for (x = 1; x <=a; x++){
+				
+				System.out.println();
+				
+
+				for (y = 1; y <=x; y++) {
+					
+					System.out.print(y+" ");
+					
+				}
+			}
+
+		}
+}
+
+
